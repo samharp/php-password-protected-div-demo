fb2e36e0595ca940d5ef8e01fdd7b922404a9f0a
sharper <hi@sammyharper.com>
2022-12-05 13:24:18 -0600

    changed basic stylings for page/form

--- secure.php
+++ secure.php
@@ -6,47 +6,83 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+
+    <!-- styling to make the demo look a wee bit better -->
+    <style>
+      /* reset */
+      *{
+        margin:0;
+        padding:0;
+      }
+      body{
+        height:100vh;
+        display:flex;
+        align-items:center;
+        justify-content:center;
+      }
+      .form-container{
+        padding:20px;
+        border:1px solid;
+      }
+      .form-row{
+        margin-bottom:20px;
+      }
+      .form-row:last-child{
+        margin-bottom:0px;
+      }
+      .hidden-content-container{
+        margin-top:50px;
+        background-color:lightgreen;
+        border:1px solid;
+        padding:20px;
+      }
+    </style>
   </head>
 
   <body>
-    
-  <!-- container for password-entry form -->
-    <div class="form-container">
 
-      <?php
-        $passErr = "";
-        $thePassword = "password";
-        $pass = "";
+    <!-- centered content -->
+    <section>
+      <!-- container for password-entry form -->
+      <div class="form-container">
+        <?php
+          $passErr = "";
+          $thePassword = "password";
+          $pass = "";
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-          if (empty($_POST["pass"])) {
-            $passErr = "Password is required";
-          } else {
-            $pass = $_POST["pass"];
-            if($pass != $thePassword){
-              $passErr = "Please enter the correct password";
+          if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($_POST["pass"])) {
+              $passErr = "Password is required";
+            } else {
+              $pass = $_POST["pass"];
+              if($pass != $thePassword){
+                $passErr = "Incorrect password";
+              }
             }
           }
-        }
-      ?>
+        ?>
 
-      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" autocomplete="off">  
-        Password: <input type="password" name="pass" value="">
-        <input type="submit" name="submit" value="Enter">
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" autocomplete="off">
+          <div class="form-row">
+            <label>Password: </label>
+            <input type="password" name="pass" value="">
+            <input type="submit" name="submit" value="Enter">
+          </div>
+          
+          <div class="form-row">
+            <?php echo $passErr;?>
+          </div>
 
-        <?php echo $passErr;?>
-      </form>
-    </div>
+        </form>
+      </div>
 
-    <!-- where the hidden content will be injected -->
-    <div class="hidden-content-container">
+      <!-- where the hidden content will be injected -->
       <?php
         if($pass == $thePassword)
         {
           include("secure.html");
         }
       ?>
-    </div>
-
+    </section>
   </body>
 </html>
